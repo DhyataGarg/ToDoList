34c525e439dd30e063e459fda297b50925b67ab6
DhyataGarg <dhyatagarg@gmail.com>
2022-12-04 22:30:44 +0530

    Send email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,23 +1,51 @@
 require("dotenv").config();
-const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SG_API_KEY);
+var nodemailer = require('nodemailer');
 
 const sendEmail = (to, from, subject, text) => {
-  const msg = {
-    to,
-    from,
-    subject,
-    html: text,
-  };
+  
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'dhyatagarg09@gmail.com',
+    pass: 'crmhhbwjhwexouhw'
+  }
+});
 
-  sgMail.send(msg, function (err, info) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Email sent successfully.");
-    }
-  });
+var mailOptions = {
+  from,
+  to,
+  subject,
+  html: text
 };
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+}
+// const sgMail = require("@sendgrid/mail");
+
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// const sendEmail = (to, from, subject, text) => {
+//   const msg = {
+//     to,
+//     from,
+//     subject,
+//     html: text,
+//   };
+
+//   sgMail.send(msg, function (err, info) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Email sent successfully.");
+//     }
+//   });
+// };
 
 module.exports = sendEmail;
